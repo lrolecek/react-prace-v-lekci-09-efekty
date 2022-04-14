b685e9c9de7637ca78098d4f3565787a10d2120a
Luděk Roleček <ludek@rolecek.cz>
2022-04-14 19:27:11 +0200

    Aktualizace podkladů

---
+++ src/components/Komponenta/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Komponenta = () => {
+
+	return (
+		<div className="komponenta">
+			<h3>Komponenta</h3>
+			<p>Tady asi něco bude</p>
+		</div>
+	)
+}
+
+export default Komponenta;
